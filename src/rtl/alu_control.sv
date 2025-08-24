da6ae8c569393030a8ebbac83c010b5a62dabb88
shaian <shaiancanada@gmail.com>
2025-08-24 16:45:47 -0400

    ALU Sub opcode fix

--- src/rtl/alu_control.sv
+++ src/rtl/alu_control.sv
@@ -8,11 +8,11 @@ module alu_control (
     always_comb begin
         case (aluop)
             2'b00: alu_control = 4'b0010; // ADD (for lw/sw)
-            2'b01: alu_control = 4'b0110; // SUB (for beq)
+            2'b01: alu_control = 4'b0011; // SUB (for beq)
             2'b10: begin // R-type
                 case ({funct7, funct3})
                     {7'b0000000, 3'b000}: alu_control = 4'b0010; // ADD
-                    {7'b0100000, 3'b000}: alu_control = 4'b0110; // SUB
+                    {7'b0100000, 3'b000}: alu_control = 4'b0011; // SUB
                     {7'b0000000, 3'b111}: alu_control = 4'b0000; // AND
                     {7'b0000000, 3'b110}: alu_control = 4'b0001; // OR
                     default: alu_control = 4'b1111; // Invalid
