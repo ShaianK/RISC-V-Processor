d21ff8a1b6e7263d3cb8a7530cfb0dfab586cdab
shaian <shaiancanada@gmail.com>
2025-09-07 13:26:22 -0400

    Refactor ALU operation definitions to use local parameters instead of typedef enum

--- src/rtl/alu.sv
+++ src/rtl/alu.sv
@@ -1,21 +1,19 @@
-typedef enum logic [3:0] {
-    ALU_AND  = 4'b0000,
-    ALU_OR   = 4'b0001,
-    ALU_ADD  = 4'b0010,
-    ALU_SUB  = 4'b0011,
-    ALU_XOR  = 4'b0100,
-    ALU_SLL  = 4'b0101,
-    ALU_SRL  = 4'b0110,
-    ALU_SLT  = 4'b0111
-} alu_op_t;
-
 module alu(
     input  logic    [31:0]  data1,
     input  logic    [31:0]  data2,
-    input  alu_op_t         aluop,
+    input  logic    [3:0]   aluop,
     output logic    [31:0]  alu_result,
     output logic            zero
 );
+
+    localparam ALU_AND = 4'b0000;
+    localparam ALU_OR  = 4'b0001;
+    localparam ALU_ADD = 4'b0010;
+    localparam ALU_SUB = 4'b0011;
+    localparam ALU_XOR = 4'b0100;
+    localparam ALU_SLL = 4'b0101;
+    localparam ALU_SRL = 4'b0110;
+    localparam ALU_SLT = 4'b0111;
 
     always_comb begin
         case (aluop)
